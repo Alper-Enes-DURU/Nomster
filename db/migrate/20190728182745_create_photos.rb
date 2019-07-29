6695415d710b75f1dab3ae1cc595e656721d367c
Alper-Enes-DURU <enesduru@gmail.com>
2019-07-29 20:05:29 +0000

    Tried to define @photo but couldn't

--- db/migrate/20190728182745_create_photos.rb
+++ db/migrate/20190728182745_create_photos.rb
@@ -5,5 +5,7 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
       t.integer :place_id
       t.timestamps
     end
+    add_index :comments, [:user_id, :place_id]
+    add_index :comments, :place_id
   end
 end
