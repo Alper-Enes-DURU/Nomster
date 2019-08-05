19116c3ee9dd8bf8b1b6224db26529aff961224e
Alper-Enes-DURU <enesduru@gmail.com>
2019-08-05 17:04:22 +0000

    Lesson 39 completed. I can upload images successfully and didn't see any error.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,8 +8,7 @@ class PhotosController < ApplicationController
 
       redirect_to place_path(@place)
     end 
-  
-  
+
     private
   
     def photo_params
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,3 @@
 class Photo < ActiveRecord::Base
-  mount_uploader :picture, PictureUploader
-  belongs_to :user
-  belongs_to :place
+  mount_uploader :picture, PictureUploader 
 end
